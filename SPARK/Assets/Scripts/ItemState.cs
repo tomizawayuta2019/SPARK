62272f5c151026cc66eae44d89abeea25ba4973e
富澤勇太 <tomizawayuta2019@gmail.com>
2018-11-02 14:15:32 +0900

    背景の動作作成 ２D用マテリアル設定 アイテム情報表示作成

---
+++ SPARK/Assets/Scripts/ItemState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// アイテムのイベントリスト（仮）
+/// </summary>
+public enum ItemEvent
+{
+    TransformItem,//アイテムが変化する
+    GimmickEnter,//ギミックを作動させる
+}
+
+//仮のアイテム構造体
+public struct ItemState
+{
+    public string itemName;
+    public int itemID;
+
+    public Sprite sprite;
+    public string itemText;
+
+    /// <summary>
+    /// このアイテムに対して使用出来るアイテムID
+    /// </summary>
+    public int needItemID;
+
+    /// <summary>
+    /// 引数のアイテムが使用可能か否か
+    /// </summary>
+    /// <param name="itemID"></param>
+    /// <returns></returns>
+    public bool IsCanUseItem(int itemID)
+    {
+        return needItemID == itemID;
+    }
+}
