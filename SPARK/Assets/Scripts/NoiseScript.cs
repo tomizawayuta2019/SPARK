ef338dba2f0a1ed4458f96c24d5bf4f7cd5277e4
富澤勇太 <tomizawayuta2019@gmail.com>
2018-11-02 17:47:26 +0900

    テスト用

---
+++ SPARK/Assets/Scripts/NoiseScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseScript : SingletonMonoBehaviour<NoiseScript> {
+
+    protected override void Awake() {
+		base.Awake();
+	}
+
+	protected override void OnDestroy() {
+		base.OnDestroy();
+	}
+
+}
