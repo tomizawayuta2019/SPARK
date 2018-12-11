96733f39a6da257aa0573f9334cbe7cff8c713ef
富澤勇太 <tomizawayuta2019@gmail.com>
2018-12-11 13:33:05 +0900

    プレイヤーに対するライトの影響を反映

--- SPARK/Assets/Script/PlayerAnimController.cs
+++ SPARK/Assets/Script/PlayerAnimController.cs
@@ -6,11 +6,31 @@ public class PlayerAnimController : MonoBehaviour {
     [SerializeField]
     Animator anim;
 
+    [SerializeField]
+    List<SpriteSet> sprites;
+
+    [SerializeField]
+    SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    SpriteRenderer lightLayer;
+
+    [System.Serializable]
+    struct SpriteSet {
+        public Sprite[] sprites;
+    }
+
     public void SetBool(string name,bool value) {
         anim.SetBool(name, value);
     }
 
     public void SetTrigger(string name) {
         anim.SetTrigger(name);
+    }
+
+    private void LateUpdate()
+    {
+        //光の影響を受ける用レイヤーの画像更新
+        lightLayer.sprite = spriteRenderer.sprite;
     }
 }
