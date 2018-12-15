099b814ad2f1fd6661d60714acf89dd2f823add0
KimitaSora <kamaaya1118@gmail.com>
2018-12-15 13:36:55 +0900

    ブローチ完成、カラスあとは素材来たらはばたくアニメーション入れるだけ

---
+++ SPARK/Assets/Script/Brooch.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brooch : GimmickKind
+{
+    private Vector2 offset; //　初期位置
+    // timeSpeedは速さ、ratioは割合（どこで落ちるか）、angleは基準の角度
+    public float timeSpeed, ratio, angle;　// おすすめ(0.1,0.5,30)
+    [SerializeField]
+    GameObject Clow;
+    private void Start()
+    {
+        //デバッグ用
+        offset = transform.position;
+        
+    }
+    //デバッグ用
+    public override void Click()
+    { 
+        //StartCoroutine(PosMove(Clow.transform.position));
+    }
+    /// <summary>
+    /// プレイヤーの位置からブローチを投げる
+    /// </summary>
+    /// <param name="vec">playerの位置</param>
+    public void ThrowBrooch(Vector2 vec)
+    {
+        offset = vec;
+        // ブローチの位置をプレイヤーの位置に
+        transform.position = vec;
+
+        StartCoroutine(PosMove(Clow.transform.position));
+
+    }
+
+    /// <summary>
+    /// ブローチを目的の位置に移動させる
+    /// </summary>
+    /// <param name="vec">目的の位置</param>
+    /// <returns></returns>
+    IEnumerator PosMove(Vector2 vec)
+    {
+        // 0が初期値　1が終点　全体の割合   
+        float t = 0f;
+        //目的地から現在地の差分
+        Vector2 P2 = vec - offset;
+        // 総移動距離を求める（直線）
+        float distance = Vector2.Distance(offset, new Vector2(vec.x,vec.y));
+        // 最大角度
+        float max_angle = 50f;
+        // 基準の距離
+        float base_range = 5f;
+        // 実際の角度 = 基準の角度 * 実際の距離 / 基準の距離
+        angle = angle * distance / base_range;
+        if (angle > max_angle)
+        {
+            angle = max_angle;
+        }
+        float P1x = P2.x * ratio;
+        //angle * Mathf.Deg2Rad 角度からラジアンへ変換
+        float P1y = Mathf.Sin(angle * Mathf.Deg2Rad) * Mathf.Abs(P1x) / Mathf.Cos(angle * Mathf.Deg2Rad);
+        Vector2 P1 = new Vector2(P1x, P1y);
+        // 終点についたらおしまい
+        while (t <= 1)
+        {
+            // [x,y]=(1–t)2P0+2(1–t)tP1+t2P2
+            float Vx = 2 * (1f - t) * t * P1.x + Mathf.Pow(t, 2) * P2.x + offset.x;
+            float Vy = 2 * (1f - t) * t * P1.y + Mathf.Pow(t, 2) * P2.y + offset.y;
+            transform.position = new Vector2(Vx, Vy);
+            // １秒あたりのtの変化量 *1フレーム
+            t += 1 / distance / timeSpeed * Time.deltaTime;
+
+            yield return null;
+        }
+        //カラスを飛ばすよ
+        Clow.GetComponent<Clows>().FlyAway();
+        gameObject.SetActive(false);
+    }
+    
+    
+}
---
+++ SPARK/Assets/Script/Clows.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * カラスだよ
+ */ 
+public class Clows : GimmickKind {
+    //
+    Vector3 playerPos;
+    [SerializeField]
+    GameObject clowsObj;
+    public enum Fly
+    {
+        left = 1,
+        up,
+        right,
+    }
+
+    private struct Bird
+    {
+        public GameObject clow;
+        public Fly type;
+        public Animator Anime;
+    }
+
+    List<Bird> birds =new List<Bird>();
+
+
+    private void Start()
+    {
+        int count = 0;
+        foreach (Transform obj in clowsObj.transform)
+        {
+            birds.Add(new Bird() {
+                clow = obj.gameObject,
+                type = (count < 3) ? Fly.left : (count < 8) ? Fly.up : Fly.right,
+                Anime = obj.gameObject.GetComponent<Animator>() });
+            count++;
+        }
+        
+    }
+
+    public override void Click()
+    {
+        FlyAway();
+        //SEController.instance.PlaySE(SEController.SEType.bird);
+        //playerPos = PlayerController.instance.gameObject.transform.position;
+
+    }
+    // これで動かす
+    public void ClowMoves()
+    {
+        FlyAway();
+    }
+
+
+    public void FlyAway()
+    {
+        float time = 0f;
+        foreach(Bird bird in birds)
+        {
+           
+            Debug.Log("obj="+bird.clow +"\nfloat=" + (float)bird.type);
+            time = Random.Range(0.0f,1.0f);
+            StartCoroutine(FlyClow(time, bird));
+        }
+    }
+    IEnumerator FlyClow(float wait,Bird bird)
+    {
+        
+        yield return new WaitForSeconds(wait);
+        if ((float)bird.type <= 1) { bird.clow.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().flipX = true; }
+        bird.Anime.SetInteger("FlyType", (int)bird.type);
+        yield return new WaitForSeconds(1);
+        bird.clow.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().flipX = false;
+
+
+    }
+}
+
