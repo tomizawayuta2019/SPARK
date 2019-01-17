5feb1e0c27a2966024a9541170a259c067b99c35
Hirokawa <17d3170021hm@edu.tech.ac.jp>
2019-01-18 02:56:07 +0900

    タグ対応、スキップ機能

---
+++ SPARK/Assets/Scripts/ADVParts/ScriptableObject/TagBox.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TAGMODE
+{
+    NONE = 0,
+    COLOR
+}
+
+[CreateAssetMenu(menuName ="SPARK/Create TagBox", fileName ="TagBox")]
+public class TagBox : ScriptableObject
+{
+    [Header("終点タグの設定")]
+    [Header("簡略化タグ")]
+    public string endTag_Simple;
+
+    [Header("色指定タグの設定")]
+    public List<ColorTag> colorTag = new List<ColorTag>();
+    [Header("色の終点タグ")]
+    public string ColorEndTag;
+
+    private TAGMODE tagMode = TAGMODE.NONE;
+
+    public string TagCheck(string tag)
+    {
+        //ENDタグだったらENDを素材そのまま返す
+        if(endTag_Simple == tag)
+        {
+            return endTag_Simple;
+        }
+
+        for(int i = 0; i < colorTag.Count; i++)
+        {
+            if(colorTag[i].simpleTag == tag)
+            {
+                tagMode = TAGMODE.COLOR;
+                return colorTag[i].regularTag;
+            }
+        }
+        
+        return null;
+    }
+}
+
+[System.Serializable]
+public class TagSet
+{
+    [Header("簡略化タグ")]
+    public string simpleTag;
+    [Header("正規タグ")]
+    public string regularTag;
+}
+
+[System.Serializable]
+public class ColorTag : TagSet
+{
+    [Header("色")]
+    public Color color;
+}
