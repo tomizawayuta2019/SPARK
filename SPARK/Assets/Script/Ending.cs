1429bc62bc6f1de19f63f9e03a9f464dcd3c94c9
KimitaSora <kamaaya1118@gmail.com>
2019-01-22 12:07:42 +0900

    エンディングムービーの調整

--- SPARK/Assets/Script/Ending.cs
+++ SPARK/Assets/Script/Ending.cs
@@ -22,26 +22,36 @@ public class Ending : SingletonMonoBehaviour<Ending> {
         Color mc = endSP.color;
         int count = 0;
         float alpha = 1;
+
+        yield return new WaitForSeconds(60.0f*Time.deltaTime);
         //Debug.Log(gameObject.transform.localPosition.y);
+        while (gameObject.transform.localPosition.y < 10f) { 
+            gameObject.transform.Translate(0, 2.5f * Time.deltaTime, 0);
+            nextImage.gameObject.transform.position = gameObject.transform.position;
+            yield return new WaitForSeconds(Time.deltaTime);
+
+        }
         nextImage.GetComponent<SpriteRenderer>().sprite = endMovies[count+1];
         while (count<12)
         {   
             while (alpha >= 0)
             {
                 endSP.color = new Color(mc.r, mc.g, mc.b, alpha);
-                alpha -= (count > 2)? 2*Time.deltaTime:0.5f*Time.deltaTime;
-                if (gameObject.transform.localPosition.y< 10f)
-                {
-                    gameObject.transform.Translate(0,3*Time.deltaTime,0);
-                    nextImage.gameObject.transform.position = gameObject.transform.position;
-                } 
+                alpha -= (count > 2)? 2*Time.deltaTime:Time.deltaTime;
+                
                 yield return new WaitForSeconds(Time.deltaTime);
             }
             count++;
             alpha = 1;
             nextImage.GetComponent<SpriteRenderer>().sprite =(count!=12) ? endMovies[count + 1]: endMovies[count];
+            if(count == 11)
+            {
+                nextImage.GetComponent<SpriteRenderer>().color = Color.black;
+            }
             endSP.sprite = endMovies[count];
         }
+
+        
         endSP.sprite = endMovies[12];
 
         //2秒
