b0b242230aa7c2d653acc1aba4223d791453c251
富澤勇太 <tomizawayuta2019@gmail.com>
2019-01-29 17:50:14 +0900

    行き止まりADVが途中で中断された際のバグ修正 ピアノのボタンがマウスオーバーしたときのみ出るよう修正

--- SPARK/Assets/Scripts/Stage/ReturnADV.cs
+++ SPARK/Assets/Scripts/Stage/ReturnADV.cs
@@ -14,20 +14,21 @@ public class ReturnADV : MonoBehaviour {
         if (isWait || collision.gameObject.tag != "Player") { return; }
 
         isWait = true;
-        ShowScript.instance.EventStart(adv);
+        ShowScript.instance.EventStart(adv, null, () => { PlayerController.instance.PlayerInputActive = false; });
 
-        StartCoroutine(ADVWait());
-    }
-
-    public IEnumerator ADVWait() {
         PlayerController.instance.PlayerInputActive = false;
-        while (ShowScript.instance.GetIsShow()) {
-            yield return null;
-        }
-        
-        PlayerController.instance.targetPosition = PlayerController.instance.transform.position + new Vector3(returnRange, 0);
-        yield return new WaitForSeconds(0.5f);
-        isWait = false;
-        PlayerController.instance.PlayerInputActive = true;
+        //StartCoroutine(ADVWait());
     }
+
+    //public IEnumerator ADVWait() {
+    //    PlayerController.instance.PlayerInputActive = false;
+    //    while (ShowScript.instance.GetIsShow()) {
+    //        yield return null;
+    //    }
+        
+    //    PlayerController.instance.targetPosition = PlayerController.instance.transform.position + new Vector3(returnRange, 0);
+    //    yield return new WaitForSeconds(0.5f);
+    //    isWait = false;
+    //    PlayerController.instance.PlayerInputActive = true;
+    //}
 }
